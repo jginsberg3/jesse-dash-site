7778832caeb5fbf74a52955b6bd8fd3bc819782c
Jesse Ginsberg <jginsberg3@gmail.com>
2018-09-19 08:23:23 -0400

    small updates when trying css and formatting

--- app.py
+++ app.py
@@ -8,11 +8,21 @@ from helpers.get_logins import *
 password_file_name = 'logins'
 login_list = return_login_info(password_file_name)
 
+# create the app
 app = dash.Dash()
+
+# add the app authentication
 auth = dash_auth.BasicAuth(
     app,
     login_list
 )
+
+# define the app's server
 server = app.server
+
+# for dev only
 app.config.suppress_callback_exceptions = True
 
+# add Boostrap CSS
+#app.css.append_css({'external_url':'https://codepen.io/chriddyp/pen/bWLwgP.css'}). # plotly official css
+app.css.append_css({'external_url':'https://codepen.io/jginsberg3/pen/zJPVBq.css'}) # jg fork of the plotly official css
--- apps/homepage.py
+++ apps/homepage.py
@@ -5,13 +5,39 @@ from dash.dependencies import Input, Output
 from app import app
 
 layout = html.Div([
+
 	html.Div([
-		html.H3('This is the awesome homepage!')
-	]),
+		html.Div([
+			html.H2('This is the awesome homepage!')
+		], className='homepagetitle'),
+
+
+		html.Div('''
+                This is an example of a simple Dash app with
+                local, customized CSS.
+            ''', className="dummy"),
+
+		html.Div([
+			html.H5('Here are some awesome web apps to try:', )
+		])
+
+	], className='row'),
+
+
 	html.Div([
-		dcc.Link('Go to App 1', href='/apps/app1')
-	]),
+		html.Div([
+			dcc.Link('Go to App 1', href='/apps/app1')
+		], className='six columns'),
+
+		html.Div([
+			dcc.Link('Go to App 2', href='/apps/app2')
+		], className='six columns')
+	], className='row'),
+
+
 	html.Div([
-		dcc.Link('Go to App 2', href='/apps/app2')
+		html.P('This is the footer.', className='footer')
 	])
+
 ])
+
